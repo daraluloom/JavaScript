5fe7ea29770807b68fa97b49bc2fa423974fe1b3
medhatelton <me.muslim.coder@gmail.com>
2020-07-21 17:22:44 +0300

    How to write Syntax in JavaScripr

---
+++ Learn/1ASyntax.js
@@ -0,0 +1,27 @@
+var x = 10 ;
+var y = 20;
+var z = 100; 
+
+// Some vars
+var c = 10 ,
+    E = 20 ,
+    Y = 200;
+
+var name =" Medhat Eltony";
+var Name =" Medhat Mohamed";
+
+var userName ; //camelCase
+
+// Simple Function
+document.getElementById("test").innerHTML = Y ;
+document.getElementById("test").innerHTML = x + y + Y ;
+console.log(name);
+console.log(Name);
+// Condition
+
+if (x +y === 40 ){
+    console.log("good");
+}else{
+    console.log("bad");
+}
+
