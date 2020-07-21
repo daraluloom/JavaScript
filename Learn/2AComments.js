ecae6d3c5b6da875ae5f4502cbb13dc6548147b6
medhatelton <me.muslim.coder@gmail.com>
2020-07-21 18:21:45 +0300

    How to write Syntax in JavaScripr Comments -  Variables

---
+++ Learn/2AComments.js
@@ -0,0 +1,21 @@
+/* global console */
+
+//Single Line comment
+
+/*
+
+Multi Line Comment Line 1
+Multi line Comment Line 2
+
+*/
+
+
+/*
+
+*******
+******
+****
+**
+*
+
+*/
---
+++ Learn/3Avariables.js
@@ -0,0 +1,46 @@
+/*
+
+Can't start  Variables like this
+start with Number 
+ 
+
+can start 
+Start with Letters , Underscore , $
+
+use camlecase in the Varibles 
+
+var = JavaScript Variable Keyword
+
+Myprice = variable Name
+
+( = )  = Assignment Operator
+
+100  = Variable VAlue  
+
+
+*/
+
+
+
+
+/* var myOldPrice = 1000;
+var myNewPrice = 900; 
+var myDiscount = myOldPrice - myNewPrice ; // 1000 - 900 = 100 */
+
+var mainPrice  = 500 ,
+    mySmallDiscount = 50 ,
+    myMediumDiscount = 100,
+    myBigDiscount = 250 ;
+
+
+document.getElementById("test").innerHTML = mainPrice; // 500
+
+document.getElementById("Prodect1").innerHTML = mainPrice -  mySmallDiscount ; // 500 - 50 = 450
+
+document.getElmentById("Product2").innerHTML = mainPrice - myMediumDiscount ;  // 500 - 100 = 400 
+
+document.getElmentById("Product3").innerHTML = mainPrice - myBigDiscount ;  // 500 - 250  = 250 
+
+
+
+
